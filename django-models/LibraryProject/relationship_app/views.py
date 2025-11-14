57bbd684e4831cc706ea0b4d65e5d419ee97a482
MaluseLek <maluselekalakala@gmail.com>
2025-11-15 00:24:17 +0200

    Add User Auth files to Dango-Models Remote Repo Directory.

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,6 +3,7 @@ from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 
