40c558c99f6f2c49f0bd373fd23d8f6879d15198
MaluseLek <maluselekalakala@gmail.com>
2025-11-14 21:32:23 +0200

    Add Dango Models Directory to remote repo.

--- django-models/LibraryProject/relationship_app/views/librarian_view.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import user_passes_test
-
-def is_librarian(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
-
-@user_passes_test(is_librarian)
-def librarian_dashboard(request):
-    return render(request, 'relationship_app/librarian_dashboard.html')
