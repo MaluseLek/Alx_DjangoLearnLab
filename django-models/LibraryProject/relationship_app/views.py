28727458b731ba73f50f0a486c64efcb4eb90495
MaluseLek <maluselekalakala@gmail.com>
2025-11-15 11:42:36 +0200

    Add Dango-Models Remote Repo Directory

---
+++ django-models/LibraryProject/relationship_app/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render, redirect
+from django.views.generic.detail import DetailView
+from .models import Book
+from .models import Library
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from django.contrib.auth.decorators import user_passes_test
+
+
+# Function-based view to display a list of books in the database
+def list_books(request):
+    books = Book.objects.all()
+    context = {'books': books}
+    return render(request, 'relationship_app/list_books.html', context)
+
+
+# Class-based view to display a list of books in the database
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+
+    # Prefetch related books to reduce DB queries
+    def get_queryset(self):
+        return Library.objects.prefetch_related('books')
+    
+
+# RegisterView using CreateView for user registration
+# Function-based view for user registration
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'relationship_app/register.html', {'form': form})
+
+"""
+# Class-Based View - RECOMMENDED
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'relationship_app/register.html'
+    success_url = reverse_lazy('login')
+
+"""
+
+
